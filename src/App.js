62afd019e7e79952fa62263fc231082db449dd91
poiuyy0420 <poiuyy258>
2019-02-23 17:13:07 +0900

    abcd

--- src/App.js
+++ src/App.js
@@ -1,80 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-/*const players = [
-  {name: 'LDK', score: 30, id:1},
-  {name: 'HONG', score: 40, id:2},
-  {name: 'KIM', score: 50, id:3},
-  {name: 'PARK', score: 60, id:4}
-];*/
+import {Header} from "./components/Header";
+import {Counter} from "./components/Counter";
+import {Player} from "./components/Player";
 
 
 const title = 'My First React Element';
 const desc = 'This is Description';
 const myTitleId = 'main-title';
 
-/*const header = (
-  <header>
-    <h1 id={myTitleId}>{title}</h1>
-    <p className="main-desc">{desc}</p>
-  </header>
-);*/
-
-const Header = (props) => {
-  console.log(props);
-  return(
-    <header>
-      <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
-    </header>
-  );
-}
-
-class Counter extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      players: 1,
-      score: 0
-    }
-  }
-
-  incrementScore = () => {
-    console.log(this);
-    /*this.setState(
-      {score: this.state.score + 1}
-    );*/
-    this.setState(prevState => ({score: prevState.score + 1}));
-  }
-
-  decrementScore = () => {
-    console.log(this);
-    /*this.setState(
-      {score: this.state.score - 1}
-    );*/
-    this.setState(prevState => ({score: prevState.score -1}));
-  }
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
-}
-
-const Player = (props) => (
-  <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-    </span>
-    <span className="player-name">{props.name}</span>
-    <Counter score={props.score} />
-  </div>
-);
 
 class App extends React.Component {
   state = {
